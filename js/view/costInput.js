fedb58511d7c49687a6c55626ad6a28811ebc434
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-15 14:33:28 +0600

    The cost of real estate. inputCost - Errors

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,6 +1,7 @@
 // import {Cleave} from "../../libs/cleaveJS/cleave-esm.min.js";
 
 function init(getData) {
+    const data = getData();
     const input = document.getElementById('input-cost');
 
     const settings = {
@@ -9,7 +10,35 @@ function init(getData) {
         delimiter: ' ',
     };
 
-    new Cleave(input, settings);
+    const cleaveInput = new Cleave(input, settings);
+    cleaveInput.setRawValue(getData().cost);
+
+    input.addEventListener('input', function () {
+        const value = +cleaveInput.getRawValue();
+
+        // Проверка мин и макс цену
+        if (value < data.minPrice || value > data.maxPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+        }
+
+        if (value >= data.minPrice && value <= data.maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+        }
+    })
+
+    input.addEventListener('change', function () {
+        const value = +cleaveInput.getRawValue();
+
+        if (value > data.maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleaveInput.setRawValue(data.maxPrice);
+        }
+
+        if (value < data.minPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleaveInput.setRawValue(data.minPrice);
+        }
+    })
 }
 
 export default init;
